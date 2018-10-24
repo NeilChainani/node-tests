0df253cae371e43445106745d7eb40a95ff09e65
Neil Chainani <Neil.Chainani@lexisnexisrisk.com>
2018-10-24 18:06:26 -0400

    Adds square method

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -5,3 +5,9 @@ it("should add two numbers", () => {
     throw new Error(`Expected 44, but got ${res}`);
   }
 });
+it("should be square of a method", () => {
+  var res = utils.square(4);
+  if (res !== 16) {
+    throw new Error(`Expected 16. but got ${res}`);
+  }
+});
